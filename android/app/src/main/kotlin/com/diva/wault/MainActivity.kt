3074c8b498f8ee2376eebed92818ea11ad644a9d
divathed3vil-sys <divathed3vil@gmail.com>
2026-03-23 18:14:04 +0530

    refactor: align MainActivity package path with app namespace

---
+++ android/app/src/main/kotlin/com/diva/wault/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.diva.wault
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- android/app/src/main/kotlin/com/example/wault/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.wault
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
