a5cbf5c696aa186c76fe095752eda85e8921f057
divathed3vil-sys <divathed3vil@gmail.com>
2026-03-23 18:50:36 +0530

    theme

---
+++ lib/theme/wault_colors.dart
@@ -0,0 +1,40 @@
+import 'dart:ui';
+
+class WaultColors {
+  WaultColors._();
+
+  static const Color background = Color(0xFF0A0A0A);
+  static const Color surface = Color(0xFF141414);
+  static const Color surfaceElevated = Color(0xFF1E1E1E);
+
+  static const Color glassWhite = Color(0x1AFFFFFF);
+  static const Color glassBorder = Color(0x14FFFFFF);
+  static const Color glassHighlight = Color(0x0DFFFFFF);
+
+  static const Color primary = Color(0xFF25D366);
+  static const Color primaryGlow = Color(0x3325D366);
+  static const Color primarySubtle = Color(0x1A25D366);
+
+  static const Color activeBlue = Color(0xFF53BDEB);
+  static const Color warning = Color(0xFFFFB340);
+  static const Color error = Color(0xFFFF6B6B);
+
+  static const Color textPrimary = Color(0xFFF5F5F5);
+  static const Color textSecondary = Color(0xB3FFFFFF);
+  static const Color textTertiary = Color(0x66FFFFFF);
+
+  static const Color whatsappDarkBg = Color(0xFF111B21);
+
+  static const Color divider = Color(0x1AFFFFFF);
+
+  static const List<Color> accentPalette = [
+    Color(0xFF25D366),
+    Color(0xFF53BDEB),
+    Color(0xFFFF6B9D),
+    Color(0xFFFFB340),
+    Color(0xFFA78BFA),
+    Color(0xFF34D399),
+    Color(0xFFF472B6),
+    Color(0xFF60A5FA),
+  ];
+}
