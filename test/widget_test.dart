10552879d259e29032f96f47ac5bbbdf0b6b07ac
divathed3vil-sys <divathed3vil@gmail.com>
2026-03-24 22:45:06 +0530

    test: use vault screen smoke test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,11 +1,13 @@
 // test/widget_test.dart
 
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:wault/main.dart';
+import 'package:wault/screens/vault_screen.dart';
 
 void main() {
-  testWidgets('WAult app smoke test', (WidgetTester tester) async {
-    await tester.pumpWidget(const WaultApp());
-    expect(find.byType(WaultApp), findsOneWidget);
+  testWidgets('WAult vault screen smoke test', (WidgetTester tester) async {
+    await tester.pumpWidget(const MaterialApp(home: VaultScreen()));
+
+    expect(find.byType(VaultScreen), findsOneWidget);
   });
 }
